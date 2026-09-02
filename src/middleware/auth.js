7ee2ee8ba3034c02b1e13dffc77755bcd18263fd
debapriya.s <debapriya.s@inspironlabs.com>
2026-09-02 10:18:12 +0530

    commit 11

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -26,4 +26,16 @@ function authorize(...roles) {
   };
 }
 
+function authorize1(...roles) {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ error: 'Not authenticated' });
+    }
+    if (roles.length > 0 && !roles.includes(req.user.role)) {
+      return res.status(403).json({ error: 'Insufficient permissions' });
+    }
+    next();
+  };
+}
+
 module.exports = { authenticate, authorize };
