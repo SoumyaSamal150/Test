4db9bc471ed27b0d035e19cb4f7f1e91a804b0e3
debapriya.s <debapriya.s@inspironlabs.com>
2026-09-02 10:17:48 +0530

    commit 9

--- src/components/App.js
+++ src/components/App.js
@@ -28,4 +28,17 @@ function App() {
   );
 }
 
+function App2() {
+  return (
+    <div className="app">
+      <Header title="Clauge Test App" />
+      <main>
+        <h2>Welcome</h2>
+        <p>This is a test application for exploring Clauge features.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
 export default App;
